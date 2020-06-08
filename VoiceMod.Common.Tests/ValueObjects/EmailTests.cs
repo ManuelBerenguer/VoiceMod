a9fc39c1471e017cb6a8bd56b3560104b321ea1a
Manuel Berenguer <manuel.berenguer@elcome.ltd.uk>
2020-06-08 10:28:10 +0200

    Email Tests

---
+++ VoiceMod.Common.Tests/ValueObjects/EmailTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VoiceMod.Common.ValueObjects;
+using Xunit;
+
+namespace VoiceMod.Common.Tests.ValueObjects
+{
+    public class EmailTests
+    {
+        [Fact]
+        public void EmptyEmailThrowsException()
+        {
+            Action email = () => Email.FromString(string.Empty);
+
+            Assert.Throws<ArgumentException>(email);
+        }
+
+        [Fact]
+        public void EmailWithInvalidFormatThrowsException()
+        {
+            Action email = () => Email.FromString("manuel.berenguer");
+
+            Assert.Throws<FormatException>(email);
+        }
+
+        [Fact]
+        public void ValidEmailIsCreatedSuccessfully()
+        {
+            var email = Email.FromString("manuel.berenguer.valero@gmail.com");
+
+            Assert.Equal("manuel.berenguer.valero@gmail.com", email.Value());
+        }
+    }
+}
